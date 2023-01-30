9e1fd2f502958696fbc0f1bc899e05c29af01d8d
Evan Hong <e.hong1424@gmail.com>
2023-01-30 20:10:52 +0000

    javascript-primitaves-and-variables - Evan Hong

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 5;
+var area = width * height;
+console.log('value of area: ' + area);
+console.log('type of area: ' + typeof area);
+
+var bill = 3;
+var payment = 10;
+var change = payment - bill;
+console.log('value of change: ' + change);
+console.log('type of change: ' + typeof change);
+
+var quizzes = 6;
+var midterm = 9;
+var final = 10;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ' + grade);
+console.log('type of grade: ' + typeof grade);
+
+var firstName = 'Evan';
+var lastName = 'Hong';
+var fullName = 'Evan ' + 'Hong'
+console.log('value of fullName: ' +  fullName);
+console.log('type of fullName: ' + typeof fullName);
+
+var pH = 9
+var isAcidic = pH < 7
+console.log('value of isAcidic: ' +  isAcidic);
+console.log('type of is Acidic: ' + typeof isAcidic);
+
+var headCount = 8
+var isSparta = headCount==300
+console.log('value of isSparta: ' +  isSparta);
+console.log('type of isSparta: ' + typeof isSparta);
+
+var motto = fullName;
+var motto = fullName + ' is the GOAT';
+console.log('value of motto: ' + motto);
+console.log('type of motto: ' + typeof motto);
