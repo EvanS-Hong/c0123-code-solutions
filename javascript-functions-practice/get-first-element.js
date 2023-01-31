1234f879dad7fc9200a527c9be4d9dee1b5be796
Evan Hong <e.hong1424@gmail.com>
2023-01-31 22:46:05 +0000

    javascript-functions-practice - Evan Hong

---
+++ javascript-functions-practice/get-first-element.js
@@ -0,0 +1,5 @@
+/* exported getFirstElement */
+function getFirstElement(array) {
+  firstElement = array[0];
+  return firstElement;
+}
---
+++ javascript-functions-practice/get-second-element.js
@@ -0,0 +1,5 @@
+/* exported getSecondElement */
+function getSecondElement(array) {
+  secondElement = array[1];
+  return secondElement;
+}
