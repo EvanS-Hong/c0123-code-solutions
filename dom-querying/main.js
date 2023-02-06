9b5926bb40ee0f579d6c8e927b1b46af4dba7bcb
Evan Hong <e.hong1424@gmail.com>
2023-02-06 18:02:51 +0000

    dom-querying - Evan Hong

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+var boo = document.querySelector("h1");
+console.log(boo);
+console.dir(boo);
+
+var boo2 = document.querySelector("#explanation");
+console.log(boo2);
+console.dir(boo2);
+
+var boo3 = document.querySelector(".hint");
+console.log(boo3);
+console.dir(boo3);
+
+var boo4 = document.querySelectorAll("p");
+console.log(boo4);
+
+var boo5 = document.querySelectorAll(".example-link");
+console.log(boo5);
