2bf99f5257a2fe538a61caff3aa39486e544a746
Evan Hong <e.hong1424@gmail.com>
2023-02-23 19:59:25 +0000

    WIP

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,10 +1,8 @@
 /* exported pick */
 function pick(source, keys) {
   var obj = {};
-  for (i = 0; i < keys.length; i++)
-  for (let x in source) {
-  if (x.includes(keys[i])); {
-    obj.push(keys[i]);
-    };
-  } return obj;
+  for (let x in keys) {
+   console.log(source[key]);
+   console.log(x)
+  }
 }
