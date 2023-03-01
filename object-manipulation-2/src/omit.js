0aa793d1f578a64de734274c04b190ed318b9757
Evan Hong <e.hong1424@gmail.com>
2023-03-01 05:04:11 +0000

    Completed Object-manipulation-2 Exersize

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,9 @@
 /* exported omit */
+function omit(source, keys) {
+  var obj = {};
+  for (let x in source) {
+    if ( !keys.includes(x)) {
+    obj[x] = source[x];
+    }
+  } return obj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,8 +1,10 @@
 /* exported pick */
 function pick(source, keys) {
   var obj = {};
+
   for (var i = 0; i < keys.length; i++) {
-    var newKey = ((keys[i] + ': ' + source[keys[i]]));
-    console.log(newKey);
-  } return obj;
+    if ( source[keys[i]] !== undefined ) {
+      obj[keys[i]] = source[keys[i]]
+    }
+  } return obj
 }
