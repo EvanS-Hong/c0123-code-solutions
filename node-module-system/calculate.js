d0c603f963d2e5141c070ac37c31cc310dd18a61
Evan Hong <e.hong1424@gmail.com>
2023-03-15 17:01:11 +0000

    added error handling if given invalid numbers

--- node-module-system/calculate.js
+++ node-module-system/calculate.js
@@ -2,13 +2,19 @@ import add from "./add.js";
 import subtract from "./subtract.js";
 import multiply from "./multiply.js";
 import divide from  "./divide.js";
-if (process.argv[3] === "plus") {
-  console.log('result: ', add(+process.argv[2], +process.argv[4]))
-  }  else if (process.argv[3] === "minus") {
-  console.log('result: ', subtract(+process.argv[2], +process.argv[4]))
-  }  else if (process.argv[3] === "times") {
-  console.log('result: ', multiply(+process.argv[2], +process.argv[4]))
-  }  else if (process.argv[3] === "divide") {
-  console.log('result: ', divide(+process.argv[2], +process.argv[4]))
-  } else { console.log(`That's not a valid operator`);
-  }
+if (isNaN(+process.argv[2])) {
+  console.log(process.argv[2] + ' is not a number.');
+} else if (isNaN(process.argv[4])) {
+  console.log(process.argv[4] + ' is not a number.');
+}  else if ((typeof (+process.argv[2]) == 'number') && (typeof (+process.argv[4]) == 'number')) {
+    if (process.argv[3] === "plus") {
+      console.log('result: ', add(+process.argv[2], +process.argv[4]))
+      }  else if (process.argv[3] === "minus") {
+      console.log('result: ', subtract(+process.argv[2], +process.argv[4]))
+      }  else if (process.argv[3] === "times") {
+      console.log('result: ', multiply(+process.argv[2], +process.argv[4]))
+      }  else if (process.argv[3] === "divide") {
+      console.log('result: ', divide(+process.argv[2], +process.argv[4]))
+      } else { console.log(`That's not a valid operator`);
+      }
+    }
