3e680e67ab08c75107759a5c22cbd29da62b6d28
Evan Hong <e.hong1424@gmail.com>
2023-03-16 23:54:56 +0000

    cleaned up code

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -4,7 +4,7 @@ import { readFile } from 'node:fs/promises';
 try {
   const filePath = new URL(process.argv[2], import.meta.url);
   const contents = await readFile(filePath, { encoding: 'utf8' });
-  await writeFile('copied-note.txt', (contents + '\n'), 'utf8');
+  await writeFile(`copied-${process.argv[2]}`, (contents + '\n'), 'utf8');
 } catch (err) {
   console.error(err.message);
 }
