6f24691b699ae341f6a2e08f769290bbc2c0c3f9
Evan Hong <e.hong1424@gmail.com>
2023-03-16 23:30:09 +0000

    clean up?

--- node-fs-writefile/note-to-self-promise.js
+++ node-fs-writefile/note-to-self-promise.js
@@ -1,11 +1,7 @@
 import { writeFile } from 'node:fs/promises';
 import { Buffer } from 'node:buffer';
 try {
-  const controller = new AbortController();
-  const { signal } = controller;
-  const data = new Uint8Array(Buffer.from('Hello Node.js'));
-  const promise = writeFile('promise-note.txt', (process.argv[2] + '\n'), 'utf8');
-
+await writeFile('promise-note.txt', (process.argv[2] + '\n'), 'utf8');
 } catch (err) {
   console.error(err);
 }
