0e0b76272a748cfadb6f9b223bccf0a484222121
Evan Hong <e.hong1424@gmail.com>
2023-03-20 17:46:05 +0000

    Cleaned up JS by cleaning up not needed code

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -6,8 +6,8 @@ async function newJsonData() {
   return JSON.parse(await readFile("./data.json"));
 
 }
-async function creation() {
-  return await writeFile("./data.json", JSON.stringify(jsonData, null, 2), 'utf8');
+async function creation(data) {
+  return await writeFile("./data.json", JSON.stringify(data, null, 2), 'utf8');
 }
 
 if (process.argv[2] === "read") {
@@ -22,33 +22,27 @@ if (process.argv[2] === "read") {
 } else if (process.argv[2] === "create") {
     try {
       const jsonData = await newJsonData();
-      let noteArray = Object.entries(jsonData.notes).map(([k, v]) => ({ [k]: v }));
-      noteArray.push({[jsonData.nextId]: `${process.argv[3]}`});
-      let newObj = {}
-      for (let x = 0; x < noteArray.length; x++) {
-        Object.assign(newObj, noteArray[x]);
-      }
-      jsonData.notes = newObj
+      jsonData.notes[jsonData.nextId] = process.argv[3];
       jsonData.nextId = jsonData.nextId + 1;
-      const newFile = await creation();
-      console.log(newFile);
+      const newFile = await creation(jsonData);
     } catch (err) {
       console.log(err);
     }
 } else if (process.argv[2] === "update") {
   try {
-  const jsonData = await newJsonData();
-  let currentNumber = process.argv[3];
+    const jsonData = await newJsonData();
+    let currentNumber = process.argv[3];
     jsonData.notes[currentNumber] = process.argv[4];
-    const newFile = await creation();
+    await creation(jsonData);
   } catch (err) {
    console.log(err);
   }
 } else if (process.argv[2] === "delete") {
   try {
-  const jsonData = await newJsonData();
-  let currentNumber = process.argv[3];
-    delete jsonData.notes[currentNumber]
+    const jsonData = await newJsonData();
+    let currentNumber = process.argv[3];
+    delete jsonData.notes[currentNumber];
+    await creation(jsonData)
   } catch (err) {
   console.log(err);
   }
