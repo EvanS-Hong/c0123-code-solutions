2fd0602d01bf2cad55ed8515eb3a7493fabd4e85
Evan Hong <e.hong1424@gmail.com>
2023-03-20 19:17:00 +0000

    fixed bad error

--- express-intro/server.js
+++ express-intro/server.js
@@ -1,6 +1,6 @@
 import express from './node_modules/express/index.js';
 
-var app = express();
+const app = express();
 
 app.listen(8080, () => {
   console.log('express server listening on port 8080');
