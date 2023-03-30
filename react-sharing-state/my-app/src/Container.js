9b30c86450d1b25fad0b8875f2949a7d9e299305
Evan Hong <e.hong1424@gmail.com>
2023-03-30 22:47:18 +0000

    completed react-sharing-state exercise - Evan Hong

---
+++ react-sharing-state/my-app/src/Container.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+export default function Container({ items }) {
+  const [current, setCurrent] = useState(0);
+
+  function incrementIndex() {
+    setCurrent(((current - 1) + items.length) % items.length);
+  }
+
+  function decremenetIndex() {
+    setCurrent((current + 1) % items.length);
+  }
+
+  function setIndex(x) {
+    setCurrent(x);
+  }
+
+  return (
+    <div>
+      <div>{items[current]}</div>
+      <div>
+        <Button onClick={incrementIndex} text="Prev" />
+        <Buttons onClick={setIndex} count={items.length} index={current}/>
+        <Button onClick={decremenetIndex}  text="Next" />
+      </div>
+    </div>
+  );
+}
+
+function Button({ text, onClick, backgroundColor}) {
+  return <button onClick={onClick} style={{backgroundColor}} >{text}</button>;
+}
+
+function Buttons({ count, onClick, index }) {
+  const buttons = [];
+  for (let i = 0; i < count; i++) {
+    buttons.push(
+      <Button
+        key={i}
+        text={i}
+        onClick={() => onClick(i)}
+        backgroundColor={i === index ? 'lightblue' : 'white'} />)
+  }
+  return <div>{buttons}</div>;
+}
