70837d762d7b693d88c783004fa6f903f62f622d
Evan Hong <e.hong1424@gmail.com>
2023-03-30 18:11:20 +0000

    Added check and X  marks

--- react-validated-input-component/my-app/src/ValidatedInput.js
+++ react-validated-input-component/my-app/src/ValidatedInput.js
@@ -4,11 +4,14 @@ export default function ValidatedInputs() {
   const [passWord, setPassWord] = useState('');
 
   let text;
+  let icon;
   if (passWord === '') {
      text = 'A password is required';
+    icon = '\u274C';
   } else if (passWord.length < 8) {
     text = 'Your password is too short';
-  };
+    icon = '\u274C';
+  } else {icon = '\u2705';}
 
 
   return (
@@ -20,11 +23,10 @@ export default function ValidatedInputs() {
           name='passWord'
           type='password'
           value={passWord}
-          onChange={e => setPassWord(e.target.value)}/>
+            onChange={e => setPassWord(e.target.value)} /> {icon}
         </label>
       </form>
       <p> {text} </p>
     </div>
-
   )
 }
